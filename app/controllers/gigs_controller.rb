4ac389f36883a0e35d14733f4eeab3bb5c092eae
Alon Roth <aroth@lightricks.com>
2022-07-24 08:22:39 +0300

    all gigs EPs

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,2 +1,45 @@
 class GigsController < ApplicationController
+
+  def index
+    @gigs = Gig.find_by(gig_params_index)
+    render json: @gigs
+  end
+
+  def show
+    @gig = Gig.find(params[:id])
+    render json: @gig
+  end
+
+  def create
+    @gig = Gig.new(gig_params_create_or_update)
+
+    if @gig.save
+      render json: @gig, status: :created, location: @gig
+    else
+      render json: @gig.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @gig = Gig.find(params[:id])
+    gig_update_response = @gig.update(gig_params_create_or_update)
+
+    if gig_update_response
+      render json: @gig, status: :ok, location: @gig
+    else
+      render @gig.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def gig_params_create_or_update
+    params.require(:gig).require(:brand_name)
+    params.require(:gig).require(:creator_id)
+    params.require(:gig).permit(:brand_name, :creator_id)
+  end
+
+  private
+  def gig_params_index
+    params.permit(:brand_name, :creator_id)
+  end
 end
