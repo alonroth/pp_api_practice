285e9e812462052088a2892eef5a41d60d5c09cb
Alon Roth <aroth@lightricks.com>
2022-07-28 13:16:57 +0300

    rate limit by auth header

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,5 @@
 Rack::Attack.cache.store = ActiveSupport::Cache::MemCacheStore.new
 
-Rack::Attack.throttle("requests by ip", limit: 1, period: 10.seconds) do |request|
-  request.ip
+Rack::Attack.throttle("requests by ip", limit: 120, period: 60.seconds) do |request|
+  request.get_header('HTTP_AUTHORIZATION') unless request.get_header('HTTP_AUTHORIZATION').empty?
 end
