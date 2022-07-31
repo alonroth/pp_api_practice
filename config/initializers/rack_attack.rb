5c2b062a25b611f6ab06982c93b0bb236d126724
Alon Roth <aroth@lightricks.com>
2022-07-31 14:05:17 +0300

    change empty => blank to support nil values

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,5 @@
 Rack::Attack.cache.store = ActiveSupport::Cache::MemCacheStore.new
 
 Rack::Attack.throttle("requests by ip", limit: 120, period: 60.seconds) do |request|
-  request.get_header('HTTP_AUTHORIZATION') unless request.get_header('HTTP_AUTHORIZATION').empty?
+  request.get_header('HTTP_AUTHORIZATION') unless request.get_header('HTTP_AUTHORIZATION').blank?
 end
