53fa6062421fa2d2cbd67071e24d4e4e7ba05237
Alon Roth <aroth@lightricks.com>
2022-08-03 11:41:10 +0300

    update gig_spec.rb

--- spec/models/gig_spec.rb
+++ spec/models/gig_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Gig, :type => :model do
-  before do
-    @creator = Creator.create! first_name: 'First', last_name: 'Last'
-    @gig = Gig.create! creator: @creator, brand_name: 'brand name'
-  end
+  let(:creator) { Creator.create! first_name: 'First', last_name: 'Last' }
+  subject { Gig.create! creator: creator, brand_name: 'brand name' }
 
   it 'should create a gig payment once gig completes' do
-    @gig.complete!
-    expect(@gig.gig_payment).to be_an_instance_of(GigPayment)
+    subject.complete!
+    expect(subject.gig_payment).to be_an_instance_of(GigPayment)
   end
 end
